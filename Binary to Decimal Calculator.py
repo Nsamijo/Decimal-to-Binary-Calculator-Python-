044a6255a71159fdfca4d0b7f17305966298b7e3
Nathan Samijo <0961962@hr.nl>
2019-10-20 17:24:57 +0200

    Updated the license

--- Binary to Decimal Calculator.py
+++ Binary to Decimal Calculator.py
@@ -1,3 +1,17 @@
+#   Copyright [2019] [Nathan Samijo]
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 #import a color library for easteregg
 from colorama import init, Back as bk , Fore as fr
 #initialize the colorama library
@@ -64,7 +78,7 @@ while not Stop:
     while number != 0:
         if number > 1:
             numDevided = number // DEFAULT_BASE
-            left = number - (numDevided * DEFAULT_BASE)
+            left = number % DEFAULT_BASE
             number = numDevided
         else:
             binNum += "1"
